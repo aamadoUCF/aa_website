1a3c3ed15910a4ec38561386e47a762e1cf0f1a0
Andrew <aamado@knights.ucf.edu>
2022-02-12 13:29:42 -0500

    Filled out first project on projects page.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
             <img src="/assets/img/andrew_compressed.jpg">
             <figcaption>UCF - Computer Science - Class of 2021</figcaption>
           </figure>
-    <div class="tile is-parent is-10">
-      <div class="tile is-child box has-background-grey-dark">
+    <div class="tile is-parent is-10 is-vertical">
+      <div class="tile is-child notification is-danger">
         <p class="title">Welcome to my Website!</p>
-        <p>I made this website to learn about front-end development, showcase my past projects, and share more about myself compared to your standard resume.</p>
+        <p class="subtitle has-text-warning">⚠️ Please note that this website is under construction ⚠️</p>
+        <p class="subtitle">I made this website to learn about front-end development, showcase my past projects, and overall share more about myself.</p>
+      </div>
+
+      <div class="tile is-child">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
       </div>
     </div>
   `,
--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -4,27 +4,56 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-project',
   template: `
     <article>
+      <!-- Minecraft Player Data Collector -->
       <section class="section">
-        <h1 class="title has-text-white-ter">Minecraft Player Data Collecter <img src="/assets/img/heapcraftLogo.png" class="project-logo"alt="Logo"></h1>
+        <h1 class="title has-text-white-ter">Minecraft Player Data Collecter</h1>
           <p class="subtitle has-text-grey-light">Written in Java –  Aug. 2021 - Dec. 2021 </p>
-
-          <p>This project was made in part of a bigger project, <a href="https://minecraftcollab.com/">Measuring Collaboration in Minecraft</a>, my Senior Design project.
+          <p class="subtitle"><a href="https://github.com/austinconnor/MinecraftCollab/tree/Data-Plugin" target="_blank" rel="noopener noreferrer"><img src="/assets/img/githubLogo.png" alt="" class="click-logo"></a>
+                              <a href="https://minecraftcollab.com/" target="_blank" rel="noopener noreferrer"><img src="/assets/img/heapcraftLogo.png" alt="" class="click-logo"></a></p>
+          
+          <!-- Semi-detailed description of project -->
+                              <p>This project was made in part of a bigger project, <a href="https://minecraftcollab.com/">Measuring Collaboration in Minecraft</a>, my Senior Design project.
           It acted as the project's core as all other aspects of the assignment relied on the data being present. 
           Throughout the duration of this project, I led both the Software Development and Back-End teams, meaning I oversaw the progress, development, and implementation
            of this program. </p>
 
            <br>
 
-           <p>As players perform specific actions in a Minecraft server, data simultaneously was recorded in a JSON format. All recorded events contained generic data, however
+           <p>As players perform specific actions in a Minecraft server, data simultaneously is recorded and formatted in JSON. All recorded events contained generic data, however
               depending on which event was recorded, there were additional fields recorded. For example, PlayerJoinEvent would only have generic data while another event such as 
               BlockBreakEvent has the generic data and also an additional field for the type of block they placed.</p>
 
             <br>
 
-            <dl class="has-text-white-ter">
-              <dt>- </dt>
-            </dl>
+            <div class="tile is-ancestor">
+              <div class="tile is-parent">
+                <div class="tile is-child">
 
+                <!-- My contributions to the project -->
+                <dl class="has-text-white-ter">
+                  <dt>- Recorded player actions such as player location, world, world time, and event type.</dt>
+                  <dt>- Cached data locally to then send to MongoDB.</dt>
+                  <dt>- Developed the program to be modular and easy to use for future researcher use. </dt>
+                  <dt>- Added an Activity feature that allows players to share what type of activity they are performing.</dt>
+                  <dt>- Coinfigured Maven project for this program.</dt>
+                  <dt>- Used GitHub for version control.</dt>
+                  <dt>- Oversaw software development of this project</dt>
+                </dl>
+
+                </div>
+
+                <!-- Visual example -->
+                <div class="tile is-child box has-background-grey-dark">
+
+                  <figure>
+                    <img src="/assets/img/data-overview.png" alt="Data Process Overview">
+                    <figcaption class="has-text-white-ter">Visual example of program's function.</figcaption>
+                  </figure>
+
+                </div>
+
+              </div>
+            </div>
       </section>
     </article>
   `,
@@ -46,6 +75,17 @@ import { Component, OnInit } from '@angular/core';
       display: inline-block;
       position: absolute;
       margin-left: 10px;
+    }
+    .click-logo
+    {
+      max-width: 2%;
+      display: inline-block;
+      margin-right: 20px;
+
+    }
+    figcaption
+    {
+      text-align: center;
     }
   `]
 })
