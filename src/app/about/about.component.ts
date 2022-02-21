640a7e35e5348024634f78d34f8b76962fc0ff73
Andrew <aamado@knights.ucf.edu>
2022-02-20 23:03:05 -0500

    Fleshed out the about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,16 +8,29 @@ import { Component, OnInit } from '@angular/core';
           <figure class="image">
             <img src="/assets/img/andrew_compressed.jpg">
             <figcaption>UCF - Computer Science - Class of 2021</figcaption>
+            <figcaption>
+              <a href="https://github.com/aamados" target="_blank" rel="noopener noreferrer"><img src="/assets/img/githubLogo.png" alt="" class="click-logo"></a>
+              <a href="mailto:aamado@knights.ucf.edu" target="_blank" rel="noopener noreferrer"><img src="/assets/img/mailLogo.png" alt="" class="click-logo"></a>
+              <a href="/assets/info/resume.pdf" target="_blank" rel="noopener noreferrer"><img src="/assets/img/resumeLogo.png" alt="" class="click-logo"></a>
+            </figcaption>
           </figure>
     <div class="tile is-parent is-10 is-vertical">
       <div class="tile is-child notification is-danger">
         <p class="title">Welcome to my Website!</p>
         <p class="subtitle has-text-warning">⚠️ Please note that this website is under construction ⚠️</p>
-        <p class="subtitle">I made this website to learn about front-end development, showcase my past projects, and overall share more about myself.</p>
+        <p class="subtitle">In the CS field, it is important to spend time learning about new technologies and how you can use them. 
+          That is why I made this website to learn about front-end development, showcase my past projects, and overall share more about myself.</p>
       </div>
 
       <div class="tile is-child">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+        <p class="subtitle">I began my software engineering journey in 2014 when I started creating server plugins for Minecraft. 
+          I always enjoyed learning about Java and spending time creating my programs. I couldn't describe to you how I felt once 
+          everything worked as intended. That gratification is what motivated me to pursue software engineering as a career.</p>
+
+          <p class="subtitle">On December 18, 2021, I became one step closer to achieving that goal as I had finally graduated from the UCF. 
+            All the hours spent studying, struggling, and connecting with others made it all worth the journey.</p>
+
+            <p class="subtitle">Feel free to browse this website for additional information about myself.</p>
       </div>
     </div>
   `,
@@ -37,6 +50,15 @@ import { Component, OnInit } from '@angular/core';
     {
       text-align: center;
       color: #eee
+    }
+
+    .click-logo
+    {
+      max-width: 15%;
+      display: inline-block;
+      margin-right: 20px;
+      margin-left: 20px;
+      margin-top: 5px;
     }
   `]
 })
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -14,6 +14,7 @@ import { Component, OnInit } from '@angular/core';
   styles: [`
   .footer
   {
+    margin-top: 20px;
     font-size: small;
     background-color: #363636;
   }  
